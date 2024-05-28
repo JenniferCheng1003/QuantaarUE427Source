2970688aaaedf5a2fd3a01d0bb417afc65a7b629
Momo Kami Cheng <57976226+BastdreStudio@users.noreply.github.com>
2024-05-28 22:29:55 +0800

    Revert "ReviewSourceCode"
    This reverts commit 6d05f598679e744c9043e0c9997e808656664237.

--- Source/FightingVR/Private/Weapons/FightingVRDamageType.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Weapons/FightingVRDamageType.h"
-#include "FightingVR.h"
-
-UFightingVRDamageType::UFightingVRDamageType(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-}
